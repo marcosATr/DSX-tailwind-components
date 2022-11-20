d6022b4f80ac8d199caa74d233651a8e56301a67
marcosATr <marcos.trannin@gmail.com>
2022-11-20 18:18:58 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+
+function App() {
+  return <div className="text-sm text-[blue] uppercase font-bold">oioioioi</div>;
+}
+
+export default App;
---
+++ src/lib/index.ts
@@ -0,0 +1,1 @@
+// export { default as AwesomeHeader } from './components/AwesomeHeader.vue'
