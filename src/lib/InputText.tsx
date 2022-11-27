c227246b00fc1a049240d58bed6a7112a5a29c80
marcosATr <marcos.trannin@gmail.com>
2022-11-27 14:21:30 -0300

    feat(input): add state to controlled input

--- src/lib/InputText.tsx
+++ src/lib/InputText.tsx
@@ -1,10 +1,12 @@
 interface InputTextProps {
   placeholder?: string;
   disabled?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
 }
 function InputText({ placeholder = '', disabled }: InputTextProps) {
   return (
-    <input disabled={disabled} type="text" placeholder={placeholder} className="border border-lightBorder focus:border-primary focus:border focus-visible:border-0 min-w-[200px] h-8 px-3 py-2 placeholder:text-lightText text-defaultText rounded font-normal text-sm leading-6 disabled:bg-gray" />
+    <input disabled={disabled} type="text" placeholder={placeholder} className="border border-lightBorder focus:border-primary focus:border focus-visible:border-0 min-w-[200px] w-full h-8 px-3 py-2 placeholder:text-lightText text-defaultText rounded font-normal text-sm leading-6 disabled:bg-gray" />
   );
 }
 
--- src/stories/InputText.stories.tsx
+++ src/stories/InputText.stories.tsx
@@ -1,19 +1,28 @@
 import { InputText } from "../lib";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { useState } from "react";
 
 export default {
   title: "InputText",
   component: InputText,
 } as ComponentMeta<typeof InputText>;
 
+
+
 export const TextInputWithoutPlaceholder: ComponentStory<typeof InputText> = () => (
   <InputText />
 );
 
 
-export const TextInputWithPlaceholder: ComponentStory<typeof InputText> = () => (
-  <InputText placeholder="Input Text" />
-);
+export const TextInputWithPlaceholder: ComponentStory<typeof InputText> = () => {
+  const [value, setValue] = useState('');
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  return <InputText placeholder="Input Text" value={value} onChange={onChange} />;
+}
+  ;
 
 export const TextInputDisabled: ComponentStory<typeof InputText> = () => (
   <InputText placeholder="Input Text" disabled />
