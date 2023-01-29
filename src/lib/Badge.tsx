aff024a99c49149f447fe6f74b51f96a759d2de8
marcosATr <marcos.trannin@gmail.com>
2023-01-29 14:13:30 -0300

    feat: replacing react-icons for badges

--- src/lib/Badge.tsx
+++ src/lib/Badge.tsx
@@ -1,27 +1,12 @@
 import React from "react";
 import { _VARIANTTYPES } from "./vars";
+import classNames from "classnames";
 
 interface BadgeProps {
   text: string | React.ReactElement;
   variant?: _VARIANTTYPES;
   version?: "solid" | "transparent";
 }
-
-const background = ({
-  variant = "primary",
-  version,
-}: Pick<BadgeProps, "variant" | "version">) => {
-  const schema = {
-    success:
-      version === "solid" ? "bg-success text-white" : "bg-success-light text-success",
-    warning:
-      version === "solid" ? "bg-warning text-white" : "bg-warning-light text-warning",
-    danger: version === "solid" ? "bg-danger text-white" : "bg-danger-light text-danger",
-    primary:
-      version === "solid" ? "bg-primary text-white" : "bg-primary-light text-primary",
-  };
-  return schema[variant];
-};
 
 export default function Badge({
   text,
@@ -30,15 +15,14 @@ export default function Badge({
 }: BadgeProps) {
   const typeString = typeof text === "string";
 
-  return (
-    <div
-      className={`flex w-fit rounded font-bold leading-3 ${typeString ? "px-[10px] py-[5px] text-xs" : "p-[5px] text-sm"
-        } ${background({
-          variant,
-          version,
-        })}`}
-    >
-      {!typeString ? React.cloneElement(text, { className: "stroke-[3]" }) : text}
-    </div>
+  const cn = classNames(
+    "flex w-fit rounded font-bold leading-3",
+    typeString
+      ? "px-[10px] py-[5px] text-xs"
+      : "p-[5px] text-sm",
+    version === "solid"
+      ? `bg-${variant} text-white`
+      : `bg-${variant}-light text-${variant}`
   );
+  return <div className={cn}>{text}</div>;
 }
--- src/stories/Badge.stories.tsx
+++ src/stories/Badge.stories.tsx
@@ -1,39 +1,79 @@
 // Alert.stories.js|jsx
 import { Badge } from "../lib";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { FiFolder } from "react-icons/fi";
+import {
+  ComponentStory,
+  ComponentMeta,
+} from "@storybook/react";
+import { Plus } from "phosphor-react";
 
 export default {
   title: "Badge",
   component: Badge,
 } as ComponentMeta<typeof Badge>;
 
-export const Primary: ComponentStory<typeof Badge> = () => <Badge text="Primary" />;
+export const Primary: ComponentStory<typeof Badge> = () => (
+  <Badge text="Primary" />
+);
 
-export const PrimaryTransparent: ComponentStory<typeof Badge> = () => (
-  <Badge text="Lighter" version="transparent" />
+export const PrimaryTransparent: ComponentStory<
+  typeof Badge
+> = () => (
+  <Badge
+    text="Lighter"
+    version="transparent"
+  />
 );
 
 export const Success: ComponentStory<typeof Badge> = () => (
-  <Badge text="Success" variant="success" />
+  <Badge
+    text="Success"
+    variant="success"
+  />
 );
 
 export const Danger: ComponentStory<typeof Badge> = () => (
-  <Badge text="Danger variant" variant="danger" />
+  <Badge
+    text="Danger variant"
+    variant="danger"
+  />
 );
 
-export const DangerLighter: ComponentStory<typeof Badge> = () => (
-  <Badge text="Another Danger" variant="danger" version="transparent" />
+export const Danger_Transparent: ComponentStory<
+  typeof Badge
+> = () => (
+  <Badge
+    text="Another Danger"
+    variant="danger"
+    version="transparent"
+  />
 );
 
 export const Warning: ComponentStory<typeof Badge> = () => (
-  <Badge text="Badge" variant="warning" />
+  <Badge
+    text="Badge"
+    variant="warning"
+  />
 );
 
-export const SingleText: ComponentStory<typeof Badge> = () => (
-  <Badge text="1" variant="warning" />
+export const SingleText: ComponentStory<
+  typeof Badge
+> = () => (
+  <Badge
+    text="1"
+    variant="warning"
+  />
 );
 
-export const ReceivingIconAsProp: ComponentStory<typeof Badge> = () => (
-  <Badge text={<FiFolder />} variant="success" />
+export const ReceivingIconAsProp: ComponentStory<
+  typeof Badge
+> = () => (
+  <Badge
+    text={
+      <Plus
+        size={14}
+        weight="bold"
+      />
+    }
+    variant="success"
+  />
 );
