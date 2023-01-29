a02aa9398babeff8ebb70dd9b92bb2ecddc72aa9
marcosATr <marcos.trannin@gmail.com>
2023-01-29 15:05:11 -0300

    feat: badge refactor

--- src/lib/Tag.tsx
+++ src/lib/Tag.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { _BACKGROUND, _VARIANTTYPES } from "./vars";
+import classNames from "classnames";
+import { _VARIANTTYPES } from "./vars";
 
 interface TagProps {
   text: string;
@@ -7,11 +7,22 @@ interface TagProps {
   rounded?: boolean;
 }
 
-function Tag({ text, variant= "primary", rounded}: TagProps) {
+function Tag({
+  text,
+  variant = "primary",
+  rounded,
+}: TagProps) {
+  const cn = classNames(
+    "h-[10px] w-[10px] mr-[6px]",
+    rounded && "rounded-full",
+    `bg-${variant}`
+  );
   return (
     <div className="flex w-fit items-center font-bold">
-      <div className={`h-[10px] w-[10px] mr-[6px] ${_BACKGROUND.solid[variant]} ${rounded && "rounded-full"}`}></div>
-      <span className="text-sm font-semibold text-dark">{text}</span>
+      <div className={cn}></div>
+      <span className="text-sm font-semibold text-dark">
+        {text}
+      </span>
     </div>
   );
 }
