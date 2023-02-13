4a8cd4101f0ea88d613e3da99c60a8ed54f2552e
marcosATr <marcos.trannin@gmail.com>
2023-02-13 00:00:14 -0300

    typography: initial implementation and twinmrege

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -8,3 +8,4 @@ export { default as InputRadio } from "./InputRadio";
 export { default as InputText } from "./InputText";
 export { default as RadioSearchCombo } from "./RadioSearchCombo";
 export { default as Tag } from "./Tag";
+export { default as Typography } from "./Typography";
