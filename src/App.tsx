5eb85818628fe87744e942717e3abe928f14c4f0
Marcos Trannin <marcos.trannin@gmail.com>
2023-02-22 11:28:45 -0300

    feat - useClickOutside

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,19 @@
-import { Breadcrumb, Typography } from "./lib";
 import {
   Link,
   RouterProvider,
   createBrowserRouter,
 } from "react-router-dom";
-const Bci = <Link to="home">home</Link>;
+import { Typography } from "./lib";
 
+function OutsideClick() {
+  return <Typography>test</Typography>;
+}
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <>
-        <Breadcrumb
-          items={[
-            { linkComponent: Bci },
-            { linkComponent: Bci },
-            { linkComponent: Bci },
-          ]}
-        />
-        <Typography>Writing is awesome.</Typography>
-        <Typography variant="display1">
-          Writing is awesome.
-        </Typography>
-        <Typography variant="display2">
-          Writing is awesome.
-        </Typography>
-        <Typography variant="display3">
-          Writing is awesome.
-        </Typography>
-        <Typography variant="display4">
-          Writing is awesome.
-        </Typography>
-        <Typography variant="h1">
-          Writing is awesome.
-        </Typography>
-        <Typography variant="h2">
-          Writing is awesome.
-        </Typography>
-        <Typography variant="h3">
-          Writing is awesome.
-        </Typography>
-        <Typography variant="h4">
-          Writing is awesome.
-        </Typography>
-        <Typography variant="h5">
-          Writing is awesome.
-        </Typography>
-        <Typography variant="h6">
-          Writing is awesome.
-        </Typography>
-        <Typography variant="lead">
-          Writing is awesome.
-        </Typography>
-        <Typography variant="paragraph">
-          Writing is awesome.
-        </Typography>
-        <Typography variant="caption">
-          Writing is awesome.
-        </Typography>
-        <Typography
-          variant="h1"
-          as="p"
-        >
-          Writing is awesome.
-        </Typography>
+        <OutsideClick />
       </>
     ),
   },
