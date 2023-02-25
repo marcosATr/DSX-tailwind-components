e1e404f86c3ef2f16adddd52cfb1f98c3372acb5
marcosATr <marcos.trannin@gmail.com>
2023-02-25 18:59:04 -0300

    feat: modal - close on click outside

--- src/lib/Modal.tsx
+++ src/lib/Modal.tsx
@@ -1,13 +1,28 @@
 import { X } from "phosphor-react";
+import { useRef } from "react";
 import { twMerge } from "tailwind-merge";
+import { useClickOutside } from "./utils";
 
-function ModalContent({ content, className, handleClose }) {
+function ModalContent({
+  content,
+  className,
+  handleClose,
+  closeOnClickOutside,
+  open,
+}) {
+  const ref = useRef(null);
+  useClickOutside(
+    ref,
+    handleClose,
+    closeOnClickOutside && open
+  );
   return (
     <>
       {/* overlay */}
       <div className="absolute top-0 left-0 z-40 h-screen w-screen bg-opacity-10"></div>
       {/* modal */}
       <div
+        ref={ref}
         className={twMerge([
           "absolute left-0 right-0 top-[30%] z-50 ml-auto mr-auto flex w-full max-w-[500px] flex-col rounded bg-white px-6 py-5 shadow",
           className,
@@ -26,13 +41,20 @@ function ModalContent({ content, className, handleClose }) {
   );
 }
 
-function Modal({ open, children, className, handleClose }) {
-  console.log(open);
+function Modal({
+  open,
+  children,
+  className,
+  handleClose,
+  closeOnClickOutside,
+}) {
   return open ? (
     <ModalContent
       content={children}
       className={className}
       handleClose={handleClose}
+      closeOnClickOutside={closeOnClickOutside}
+      open={open}
     />
   ) : null;
 }
